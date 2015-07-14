ae0fd1c6515ed6c927dacfc3da05f3a5ea2786eb
Rotek Song <songrotek@qq.com>
2015-07-14 21:01:26 +0800

    Initial Commit

---
+++ tinyCNNOSX/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tinyCNNOSX
+//
+//  Created by FloodSurge on 15/7/14.
+//  Copyright (c) 2015年 FloodSurge. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
